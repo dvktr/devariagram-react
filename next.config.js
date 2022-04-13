45eb316ab36ef92274c2cf9bfb36093922b7d876
dvktr <victormendes04012004@gmail.com>
2022-04-13 18:59:23 -0300

    adicionar imagens

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includedPaths: [path.join(__dirname, 'styles')]
+  }
 }
 
 module.exports = nextConfig
